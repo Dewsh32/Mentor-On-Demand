1449bbe6810231a15ec1ae5b990b9d85af61ca1a
Devansh <devanshg85@gmail.com>
2019-10-05 11:37:59 +0530

    new commit

--- src/app/mentor/mprofile/mprofile.component.ts
+++ src/app/mentor/mprofile/mprofile.component.ts
@@ -14,6 +14,7 @@ import {Mentor} from './mprofile.model'
 export class MprofileComponent implements OnInit {
 
 	mentor: Mentor[];
+	user=sessionStorage.getItem('username');
 	constructor(private router: Router, private mprofileService: MprofileService)
 	{
 
